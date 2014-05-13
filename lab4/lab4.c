6132d06ac104db54ccb278f4d288b47c8c1b8b31
Gustav Svensk <gussv666@zaza7.ida.liu.se>
2014-05-13 16:48:58 +0200

    started lab4

---
+++ lab4/lab4.c
@@ -0,0 +1,13 @@
+#include<physics.h>
+#include<coordinate.h>
+#include<definitions.h>
+#include<stdio.h>
+#include<mpi.h>
+
+int main(int argc, char** argv)
+{
+    if(argc != 2){
+	fprintf(stderr, "Usage: lab4 num_processes");
+	exit(1);
+    }
+}
