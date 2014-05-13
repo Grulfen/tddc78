ed177827e73191d330b0b1f637a9a38d742df8e1
Gustav Svensk <x_gussv@triolith1.(none)>
2014-05-13 17:14:26 +0200

    First few lines in lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,13 +1,14 @@
-#include<physics.h>
-#include<coordinate.h>
-#include<definitions.h>
+#include"physics.h"
+#include"coordinate.h"
+#include"definitions.h"
 #include<stdio.h>
 #include<mpi.h>
 
 int main(int argc, char** argv)
 {
     if(argc != 2){
-	fprintf(stderr, "Usage: lab4 num_processes");
+	fprintf(stderr, "Usage: lab4 num_processes\n");
 	exit(1);
     }
+    return 0;
 }
