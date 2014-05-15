4f91d93d1f3f95b07d79d837674bbaf42cbde5dc
Gustav Svensk <x_gussv@triolith1.(none)>
2014-05-15 12:04:07 +0200

    Added MPI stuff to lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -5,6 +5,7 @@
 #include<list>
 #include<cstdlib>
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
@@ -32,14 +33,55 @@ list<pcord_t> generate_particles(int n, int num_processes)
 
 int main(int argc, char** argv)
 {
-        if(argc != 3){
-                fprintf(stderr, "Usage: lab4 num_processes time_steps\n");
-                exit(1);
+
+        MPI_Status status;
+        MPI_Init(NULL, NULL);
+        MPI_Comm grid;
+
+        int myid, size;
+        int time_steps;
+        // Indicating if there are walls
+        // in order up, right, down, left
+        bool walls[4];
+
+
+        // My coordinates in grid
+        int coords[2];
+
+        // Grid is not circular
+        int period[2] = {0,0};
+
+        MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+        int side = round(sqrt(size));
+
+        // Dimensions of grid
+        int dims[2] = {side, side};
+
+        if(myid == 0){
+                if(argc != 2){
+                        cerr << "Usage: lab4 time_steps" << endl;
+                        exit(1);
+                } 
+                if(side*side != size){
+                        cerr << "Number of processes must be a square" << endl;
+                        exit(2);
+                }
+
+                time_steps = atoi(argv[1]);
         }
 
-        int time_steps = atoi(argv[2]);
+        // Broadcast time steps
+        MPI_Bcast(&time_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, 0, &grid);
+        MPI_Cart_coords(grid, myid, 2, coords);
+        cout << "Myid: " << myid << " My coords: (" << coords[0] << ", " << coords[1] << ") " << endl;
+
         float collision_time, g_pressure = 0.0;
-        cord_t wall = {0.0, BOX_HORIZ_SIZE, 0.0, BOX_VERT_SIZE};
+        // Set walls for each process
+        cord_t wall = {0.0, BOX_HORIZ_SIZE*BOX_HORIZ_SIZE/size, 0.0, BOX_VERT_SIZE*BOX_VERT_SIZE/size};
         /* srand(time(NULL)); */
 
         list<pcord_t> particles, moved, comm_up, comm_down, comm_left, comm_right;
@@ -88,6 +130,7 @@ int main(int argc, char** argv)
                 }
         }
 
+        MPI_Finalize();
         cout << "Pressure: " << g_pressure/time_steps/WALL_LENGTH << endl;
 
         return 0;
