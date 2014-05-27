3fee317ca965dc80e5f90978694585d3e2cc9a1d
Gustav Svensk <x_gussv@triolith2.(none)>
2014-05-27 13:06:01 +0200

    Started with lab5

---
+++ lab5/coordinate.h
@@ -0,0 +1,22 @@
+#ifndef _coordinate_h
+#define _coordinate_h
+
+struct cord_t {
+    float x0 ;
+    float x1 ;
+    float y0 ;
+    float y1 ;
+} ;
+
+struct pcord_t {
+    float x ;
+    float y ;
+    float vx ;
+    float vy ;
+
+} ;
+
+/* typedef struct cord cord_t ; */
+/* typedef struct part_cord pcord_t ; */
+
+#endif
